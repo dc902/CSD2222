78311f74ae12ec8ac2360ae81505adc025815dbb
eh480 <eh480@PC-00105939.uni.mdx.ac.uk>
2014-11-07 12:20:43 +0000

    Second commit

--- src/student1.java
+++ src/student1.java
@@ -3,7 +3,7 @@ public class student1 {
 	
 	public static void main(String [] args){
 	
-	student firststudent = new student ("Ray" , 19 , 'M');
+	student firststudent = new student ("Steve" , 19 , 'M');
 	firststudent.showStudent();
 }
 }
